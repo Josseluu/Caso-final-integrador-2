53a1b4ecaab547d0aee41475c12dea16e801e611
Josseluu <jslslpz@myuax.com>
2024-03-06 19:09:06 +0100

    Initial commit

--- Adminstracion de recursos/Adminstracion de recursos/Main.java
+++ Adminstracion de recursos/Adminstracion de recursos/Main.java
@@ -1,5 +1,77 @@
+import java.util.HashMap;
+import java.util.Map;
+
+
+class Recurso {
+    protected String nombre;
+    protected int cantidad;
+
+    public Recurso(String nombre, int cantidad) {
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+    }
+
+    public void mostrarInformacion() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Cantidad disponible: " + cantidad);
+    }
+}
+
+
+class Alimento extends Recurso {
+    public Alimento(String nombre, int cantidad) {
+        super(nombre, cantidad);
+    }
+}
+
+class Medicina extends Recurso {
+    public Medicina(String nombre, int cantidad) {
+        super(nombre, cantidad);
+    }
+}
+
+class Equipamiento extends Recurso {
+    public Equipamiento(String nombre, int cantidad) {
+        super(nombre, cantidad);
+    }
+}
+
+
+class ManejadorRecursos {
+    private Map<String, Recurso> inventario;
+
+    public ManejadorRecursos() {
+        this.inventario = new HashMap<>();
+    }
+
+    public void agregarRecurso(Recurso recurso) {
+        inventario.put(recurso.nombre, recurso);
+    }
+
+    public void mostrarInventario() {
+        System.out.println("Inventario del zoológico:");
+        for (Recurso recurso : inventario.values()) {
+            recurso.mostrarInformacion();
+        }
+    }
+
+
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Alimento carne = new Alimento("Carne", 100);
+        Medicina vacunas = new Medicina("Vacunas", 50);
+        Equipamiento jaulas = new Equipamiento("Jaulas", 20);
+
+
+        ManejadorRecursos manejador = new ManejadorRecursos();
+        manejador.agregarRecurso(carne);
+        manejador.agregarRecurso(vacunas);
+        manejador.agregarRecurso(jaulas);
+
+
+        manejador.mostrarInventario();
     }
 }
